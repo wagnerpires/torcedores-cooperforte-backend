7c3f13e223b22017d0e93a2bc9ad78f82eec5736
Wagner Pires <wagnerpires@hotmail.com>
2021-07-29 08:30:52 -0300

    Backend processo seletivo cooperforte

--- src/test/java/br/com/desafio/cooperforteservice/services/TorcedorServiceTests.java
+++ src/test/java/br/com/desafio/cooperforteservice/services/TorcedorServiceTests.java
@@ -1,26 +1,15 @@
-package br.com.desafio.cooperforteservice;
+package br.com.desafio.cooperforteservice.services;
 
-import br.com.desafio.cooperforteservice.entities.Torcedor;
-import br.com.desafio.cooperforteservice.exceptions.TorcedorNotFound;
 import br.com.desafio.cooperforteservice.repositories.TorcedorRepository;
-import br.com.desafio.cooperforteservice.services.TorcedorService;
-import br.com.desafio.cooperforteservice.services.exceptions.DatabaseException;
-import br.com.desafio.cooperforteservice.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Optional;
-
-import static org.mockito.Mockito.times;
-
 @ExtendWith(SpringExtension.class)
-public class ServiceTests {
+public class TorcedorServiceTests {
 
     @Mock
     private TorcedorRepository torcedorRepository;
